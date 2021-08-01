3d90695e57953f6e5787bd1e82b2b4bb369169a1
Tomas Öberg <tomaserikoberg@gmail.com>
2021-08-01 10:35:27 +0200

    Start converting def and ref providers

--- server/src/modules/definitions.ts
+++ server/src/modules/definitions.ts
@@ -4,6 +4,70 @@ import { URI } from 'vscode-uri';
 import { connection } from '../server';
 import { Tads3SymbolManager } from '../Tads3SymbolManager';
 
-export async function onDefinition(handler: DefinitionParams, symbolManager: Tads3SymbolManager) {
-	return [];
+export async function onDefinition({textDocument,position}: DefinitionParams, symbolManager: Tads3SymbolManager) {
+	
+	/*
+	const wordRange = textDocument.getWordRangeAtPosition(position);
+		const symbolName = textDocument.getText(wordRange);
+
+		for(const key of this.outlinerManager.getAllSymbols().keys()) {
+			//this.allSymbolsFlattened.push(flattenTreeToArray(this.outlinerManager.getAllSymbols().get(key)));
+			const symbol = flattenTreeToArray(this.outlinerManager.getAllSymbols().get(key))
+							.find(x=>x.name === symbolName);
+			//let symbol = this.outlinerManager.getAllSymbols().get(key).find(x=>x.name === symbolName);
+			if(symbol !== undefined) {
+				return [new vscode.Location(Uri.parse(key), symbol.range)];
+			}	
+		}
+
+		// TODO: Clean up this mess
+		for(const filePath of this.outlinerManager.getAllSymbols().keys()) {
+			// TODO: get current object from location
+			const containingObject = this.outlinerManager.getAllSymbols().get(filePath)
+				.find(x => x.range.contains(position));
+			// TODO: containingObject.children.filter(x=>x.kind === SymbolKind.Function)
+			// TODO: handle scoping before adding assignment expressions
+			
+			
+			//let assignmentSymbol = containingObject.children //this.outlinerManager.listener.assignmentStatements
+			//	.filter(x=>x['functionScope'])
+			//	.find(x=>x.name===symbolName);
+			//if(assignmentSymbol) {
+			//	return [new vscode.Location(Uri.parse(filePath), assignmentSymbol.range)];
+			//}
+			
+
+			
+			let symbol = this.outlinerManager.getAllSymbols().get(filePath)
+				.find(x => x.name === symbolName);
+
+ 		    if(symbol !== undefined) {
+				return [new vscode.Location(Uri.parse(filePath), symbol.range)];
+			}
+
+			symbol = this.outlinerManager.getAllSymbols().get(filePath)
+				.flatMap(s=>s.children).find(x => x.name === symbolName) as ExtendedDocumentSymbol;
+			if(symbol !== undefined) {
+				return [new vscode.Location(Uri.parse(filePath), symbol.range)];
+			}
+
+			// TODO: handle scoping so not property with the same in another object
+			//       gets referenced to.
+
+
+			
+			const prop = containingObject.children.find(x => x.name === symbolName);
+			//let prop = this.outlinerManager.getAllSymbols().get(filePath)
+			//	.flatMap(x=>x.children).find(x => x.name === symbolName);
+			if(prop !== undefined) {
+				return [new vscode.Location(Uri.parse(filePath), prop.range)];
+			}
+
+
+		}*/
+
+		//let firstFoundSymbol = this.outlinerManager.getSymbols(textDocument.uri.path).find(x => x.name === symbolName);
+		//return [new vscode.Location(textDocument.uri, firstFoundSymbol.range)];
+		return [];
+
 }
--- server/src/modules/references.ts
+++ server/src/modules/references.ts
@@ -4,7 +4,105 @@ import { URI } from 'vscode-uri';
 import { connection } from '../server';
 import { Tads3SymbolManager } from '../Tads3SymbolManager';
 
+const memberCallRegExp = new RegExp(/\s*(?:(.*)[.])(.*)\(\);/);
+
 
 export async function onReferences(handler: ReferenceParams, symbolManager: Tads3SymbolManager) {
-	return [];
+	const {position, textDocument} = handler;
+	const locations: [] = [];
+
+	/*
+	const word = document.getText(document.getWordRangeAtPosition(position));
+	const currentLine = document.lineAt(position.line).text;
+
+	
+	//for(let env of this.symbolManager.scopedEnvironments) {
+		//let location = env.getSymbol(word);
+		//if(location) {
+			//console.error(`word found in ${location.uri}`);
+			//locations.push(location);
+		//}
+	//}
+	
+	try {
+		// TODO: check if matching a member call
+		const memberExpression = T3ReferenceProvider.memberCallRegExp.exec(currentLine);
+		if (memberExpression && memberExpression.length > 1) {
+			const root = memberExpression[1];
+			//if(memberExpression.length)
+			console.error(`memberExpression: ${memberExpression}, root: ${root}`);
+			//TODO: add listener logic to capture text with member call expressions
+
+			// Look in 
+			const map = this.symbolManager.memberCallChains.get(document.uri.path);
+			const arrayOfMemberCallsArray = map.get(word);
+			const result = arrayOfMemberCallsArray.find(x=>x.line===position.line+1);
+			
+			if (result.memberChain.length === 1) {
+				// Locate the symbol that is the root of the call chain:
+				// TODO: make a findChildrenWithin() method to make this call easier
+				const rootSymbolName = result.memberChain[0];
+
+				//TODO: Make it global instead and use Uri.parse(path)
+					const rootSymbol = this.symbolManager.getSymbols(document.uri.path)
+					.find(x => x.name === rootSymbolName);
+				
+				const method = rootSymbol.children
+					.filter(x => x.kind === SymbolKind.Method)
+					.find(x => x.name === word);
+				if (method) {
+					console.error(method);
+					//TODO: Make it global instead and use Uri.parse(path)
+					const location = new Location(document.uri, method.range);
+					locations.push(location);
+					return locations;
+				}
+			}
+			//let o = [...map.values()].find(x=>x.line === position.line+1)
+			
+			
+			//this.symbolManager.getSymbols(document.uri.path)
+			//	.find(x => x.range.start.line === position.line);
+		}
+
+
+		for(const filePath of this.symbolManager.getAllSymbols().keys()) {
+			// TODO: get current object from location
+			const currentUri = Uri.parse(filePath);
+			const containingObject = this.symbolManager.getAllSymbols().get(filePath).find(x => x.name === word);
+			if(containingObject) {
+				const location = new Location(currentUri, containingObject.range);
+				locations.push(location);
+			}
+		}
+
+
+		for(const path of this.symbolManager.keywords.keys()) {
+			const currentUri = Uri.parse(path);
+			const localKeywordMap = this.symbolManager.keywords.get(path); // 
+			const ranges = localKeywordMap.get(word);
+			if(path && ranges) {
+				ranges.forEach(range => {
+					const location = new Location(currentUri, range);
+					locations.push(location);
+				});
+			}
+		}
+	} catch (err) {
+
+		console.error('Unexpected');
+	}*/
+	/*
+	const symbolFound = this.symbolManager.environments.get(word);
+	for (const {range,path} of symbolFound.ranges) {
+		if(path) {
+			const uri = Uri.parse(path);
+			console.log(uri);
+			const location = new Location(uri, range);
+			locations.push(location);
+		}
+	}*/
+
+	return locations;
 }
+
