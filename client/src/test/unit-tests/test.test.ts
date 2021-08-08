2e0165fed2342d19ca7ed5939b0cddb6807568d2
Tomas Öberg <tomaserikoberg@gmail.com>
2021-08-08 11:32:33 +0200

    Add unit test

---
+++ client/src/test/unit-tests/test.test.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const assert = require('assert');
+
+describe('', ()=> {
+
+	//
+	it('should work', ()=> {
+
+		assert.equal(1-1, 1);
+
+	});
+});
