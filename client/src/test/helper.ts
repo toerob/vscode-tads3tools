9d4b10242984c784f024616cfe5b016c3af0e23d
Tomas Öberg <tomaserikoberg@gmail.com>
2021-08-27 10:36:04 +0200

    Maybe fixes e2e tests

--- client/src/test/helper.ts
+++ client/src/test/helper.ts
@@ -5,6 +5,7 @@
 
 import * as vscode from 'vscode';
 import * as path from 'path';
+import { copyFileSync, fstat } from 'fs';
 
 export let doc: vscode.TextDocument;
 export let editor: vscode.TextEditor;
@@ -15,6 +16,15 @@ export let platformEol: string;
  * Activates the toerob.vscode-tads3tools extension
  */
 export async function activate(docUri: vscode.Uri) {
+
+	try {
+		copyTestFixtureFilesToTmpFolder(['completion.txt','diagnostics.t','Makefile.t3m']);
+			} catch(err) {
+		console.error(err)
+		process.exit(1);
+	}
+	
+
 	const ext = vscode.extensions.getExtension('toerob.vscode-tads3tools')!;
 	await ext.activate();
 	try {
@@ -25,13 +35,20 @@ export async function activate(docUri: vscode.Uri) {
 		console.error(e);
 	}
 }
+export async function copyTestFixtureFilesToTmpFolder(fileArray: string[]) {
+	for(const f of fileArray) {
+		copyFileSync(path.resolve(__dirname, `../../testFixture/${f}`), `/tmp/${f}`);	
+	}
+
+}
 
 export async function sleep(ms: number) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 export const getDocPath = (p: string) => {
-	return path.resolve(__dirname, '../../testFixture', p);
+	return path.resolve(__dirname, '/tmp/', p);
+	//return path.resolve(__dirname, '../../testFixture', p);
 };
 export const getDocUri = (p: string) => {
 	return vscode.Uri.file(getDocPath(p));
