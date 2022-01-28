9da685ebcd3faac8fe657d616e02db6ea914478c
Tomas Öberg <tomaserikoberg@gmail.com>
2022-01-28 17:16:22 +0100

    Guard against other kinds of project if .t files are missing

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -849,6 +849,7 @@ async function initiallyParseTadsProject() {
 		}
 		if (extensionState.getChosenMakefileUri() === undefined) {
 			client.info(`No tads3 makefile could be found`);
+
 			detectAndInitiallyParseTads2Project();
 			return;
 		}
@@ -919,6 +920,13 @@ async function detectAndInitiallyParseTads2Project() {
 	const nodes = new Map();
 	const includeRegexp = new RegExp(/[#]\s*include\s*[<"](.+)[">]/);
 	const files = await workspace.findFiles('**/*.{t,h}');
+
+	const result = files.filter(x=>x.fsPath.endsWith('.t'));
+	if(result.length === 0) {
+		client.info(`No tads source files found at all, won\'t assume Tads2 project`);
+		return;
+	}
+
 	for (const currentFile of files) {
 		const doc = await workspace.openTextDocument(currentFile.fsPath);
 		nodes.set(basename(currentFile.fsPath), new DependencyNode(currentFile));
